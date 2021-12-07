086b6f1e415c5eb64a4c3afd6b974badf4fbb1c2
mrrkramesh07 <ramesh_java@outlook.com>
2021-12-07 10:52:37 +0530

    Patient API

--- bin/www.js
+++ bin/www.js
@@ -11,7 +11,7 @@ import http from"http";
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || "3000");
+var port = normalizePort(process.env.PORT || "5050");
 app.set("port", port);
 
 /**
